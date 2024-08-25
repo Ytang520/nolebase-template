cc37daac96c44c086fa44999b5a0c6fea4b22023
Hua Tang <2023126578@sjtu.edu.cn>
2024-08-25 20:45:20 +0800

    test_03

--- .vitepress/creators.ts
+++ .vitepress/creators.ts
@@ -16,12 +16,12 @@ export interface Creator {
   emailAliases?: string[]
 }
 
-const getAvatarUrl = (name: string) => `https://github.com/${name}.png`
+// const getAvatarUrl = (name: string) => `https://github.com/${name}.png`
 
 export const creators: Creator[] = [
   {
     name: 'Frank',
-    avatar: '',
+    avatar: 'Yuki_jpg.jpg',
     username: 'Frank',
     title: '普通人',
     desc: '喜欢人类微妙的情感，喜欢知识的脉络，目前经历着疼痛，无法呼吸',
@@ -31,10 +31,11 @@ export const creators: Creator[] = [
     nameAliases: ['Frank'],
     emailAliases: ['1253522515@qq.com'],
   },
-].map<Creator>((c) => {
-  c.avatar = c.avatar || getAvatarUrl(c.username)
-  return c as Creator
-})
+]
+// .map<Creator>((c) => {
+//   c.avatar = c.avatar || getAvatarUrl(c.username)
+//   return c as Creator
+// })
 
 export const creatorNames = creators.map(c => c.name)
 export const creatorUsernames = creators.map(c => c.username || '')
